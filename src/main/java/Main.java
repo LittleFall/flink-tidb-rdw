6cee49c4810f33d397e7ced8fa8cc3053db5d6a2
Zhi Qi <1578166061@qq.com>
2020-08-12 22:15:35 +0800

    用 canal-json 解析成 catalog，将流输出到屏幕上。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,59 @@
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.table.api.bridge.java.BatchTableEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.table.factories.DeserializationFormatFactory;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.*;
+//import org.apache.flink.table.api.java;
+import org.apache.flink.types.Row;
+
+import static org.apache.flink.table.api.Expressions.*;
 
 import java.util.Date;
 import java.util.Properties;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        StreamTableEnvironment tEnv = StreamTableEnvironment.create(
+                StreamExecutionEnvironment.getExecutionEnvironment(),
+                EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build()
+        );
 
-        Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "localhost:9092");
-        properties.setProperty("group.id", "test");
+        tEnv.executeSql("CREATE TABLE mysql_t (" +
+                "id INT, " +
+                "val INT " +
+                ") WITH (" +
+                "'connector' = 'kafka'," +
+                "'topic' = 'example'," +
+                "'properties.group.id' = 'test'," +
+                "'scan.startup.mode' = 'earliest-offset'," +
+                "'properties.bootstrap.servers' = '127.0.0.1:9092'," +
+                "'format' = 'canal-json'  " +
+                ")"
+        );
+        tEnv.executeSql("CREATE TABLE print_table WITH ('connector' = 'print')" +
+        "LIKE mysql_t (EXCLUDING ALL)");
+//        tEnv.executeSql("CREATE TABLE tidb_t (\n" +
+//                "id int, " +
+//                "val int " +
+//                ") WITH (" +
+//                "   'connector'  = 'jdbc',\n" +
+//                "   'url'        = 'jdbc:mysql://mysql:4000/test',\n" +
+//                "   'table-name' = 't',\n" +
+//                "   'driver'     = 'com.mysql.jdbc.Driver',\n" +
+//                "   'username'   = 'root',\n" +
+//                "   'password'   = ''\n" +
+//                ")"
+//        );
 
-        FlinkKafkaConsumer<String> myConsumer = new FlinkKafkaConsumer<>("example", new SimpleStringSchema(), properties);
-        myConsumer.setStartFromEarliest();     // 尽可能从最早的记录开始
-
-        DataStream<String> stream = env.addSource(myConsumer);
-
-        stream.print();
-
-        env.execute();
+        Table t = tEnv.from("mysql_t");
+        t.executeInsert("print_table");
     }
 }
