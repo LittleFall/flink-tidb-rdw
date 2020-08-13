f007618ac0db91c62a2c1301adb5d040ddd2df3a
Zhi Qi <1578166061@qq.com>
2020-08-13 16:16:53 +0800

    demo 达成：流式 join 二合一。有一个 bug：插入值时会产生 null 值。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,41 +21,90 @@ import java.util.Date;
 import java.util.Properties;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
+        final String DDLCreateBase = "create table base ( \n"+
+                "id int primary key, \n"+
+                "location varchar(20) \n"+
+                ")WITH ( \n"+
+                "'connector' = 'kafka', \n"+
+                "'topic' = 'example', \n"+
+                "'properties.group.id' = 'testGroup', \n"+
+                "'scan.startup.mode' = 'latest-offset', \n"+
+                "'properties.bootstrap.servers' = 'localhost:9092', \n"+
+                "'format' = 'canal-json' \n"+
+                ")";
+        final String DDLCreateStuff = "create table stuff(\n"+
+                "id int primary key,\n"+
+                "b_id int,\n"+
+                "name varchar(20)\n"+
+                ")WITH (\n"+
+                "'connector' = 'kafka',\n"+
+                "'topic' = 'example',\n"+
+                "'properties.group.id' = 'testGroup',\n"+
+                "'scan.startup.mode' = 'latest-offset',\n"+
+                "'properties.bootstrap.servers' = 'localhost:9092',\n"+
+                "'format' = 'canal-json'\n"+
+                ")";
+        final String DDLCreateWideStuff = "create table wide_stuff(\n"+
+                "s_id int primary key,\n"+
+                "b_id int,\n"+
+                "b_location varchar(20),\n"+
+                "s_name varchar(20)\n"+
+                ") WITH (\n"+
+                "'connector'  = 'jdbc',\n"+
+                "'url'        = 'jdbc:mysql://127.0.0.1:4000/test',\n"+
+                "'table-name' = 'wide_stuff',\n"+
+                "'driver'     = 'com.mysql.cj.jdbc.Driver',\n"+
+                "'username'   = 'root',\n"+
+                "'password'   = ''\n"+
+                ")";
+
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(
                 StreamExecutionEnvironment.getExecutionEnvironment(),
                 EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build()
         );
 
-        tEnv.executeSql("CREATE TABLE mysql_t (" +
-                "id INT, " +
-                "val INT " +
-                ") WITH (" +
-                "'connector' = 'kafka'," +
-                "'topic' = 'example'," +
-                "'properties.group.id' = 'test'," +
-                "'scan.startup.mode' = 'earliest-offset'," +
-                "'properties.bootstrap.servers' = '127.0.0.1:9092'," +
-                "'format' = 'canal-json'  " +
-                ")"
-        );
-        tEnv.executeSql("CREATE TABLE print_table WITH ('connector' = 'print')" +
-        "LIKE mysql_t (EXCLUDING ALL)");
-        tEnv.executeSql("CREATE TABLE tidb_t (\n" +
-                "id int primary key, " +
-                "val int " +
-                ") WITH (" +
-                "   'connector'  = 'jdbc',\n" +
-                "   'url'        = 'jdbc:mysql://127.0.0.1:4000/test',\n" +
-                "   'table-name' = 't',\n" +
-                "   'driver'     = 'com.mysql.cj.jdbc.Driver',\n" +
-                "   'username'   = 'root',\n" +
-                "   'password'   = ''\n" +
-                ")"
-        );
+        tEnv.executeSql(DDLCreateBase);
+        tEnv.executeSql(DDLCreateStuff);
+        tEnv.executeSql(DDLCreateWideStuff);
 
-        Table t = tEnv.sqlQuery("select * from mysql_t");
-        t.executeInsert("tidb_t");
-        t.executeInsert("print_table");
+        printSource(tEnv, "base");
+        printSource(tEnv, "stuff");
+
+        tEnv.executeSql("CREATE TABLE print_wide_stuff WITH ('connector' = 'print') LIKE wide_stuff (EXCLUDING ALL)");
+
+        Table t = tEnv.sqlQuery(
+                "select distinct stuff.id s_id, base.id b_id, base.location, stuff.name\n" +
+                "from stuff inner join base\n" +
+                "on stuff.b_id = base.id\n" +
+                "where location is not null" // TODO: find why must add this condition.
+        );
+        t.executeInsert("wide_stuff");
+        t.executeInsert("print_wide_stuff");
+    }
+    /**
+     * 将源表的任何更改打印在屏幕上，原理是创建一个名为 print_source 的表
+     * @param tEnv 表环境
+     * @param source 表名：必须已经被创建，必须是 source 表
+     */
+    static void printSource(StreamTableEnvironment tEnv, String source) {
+        tEnv.executeSql("CREATE TABLE print_" + source + " WITH ('connector' = 'print') LIKE " + source + " (EXCLUDING ALL)");
+        tEnv.from(source).executeInsert("print_" + source);
     }
 }
+
+
+/*
+测试语句：
+delete from stuff;
+delete from base;
+
+insert into base values (1, 'beijing');
+insert into stuff values (1, 1, 'zz');
+insert into stuff values (2, 1, 't');
+insert into base values (2, 'shanghai');
+insert into stuff values (3, 2, 'qq');
+update stuff set name = 'qz' where id = 3;
+delete from stuff where name = 't';
+*/
