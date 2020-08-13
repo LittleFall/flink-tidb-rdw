765d12c3c29550ba224fa22239975bfb5ef4057c
Zhi Qi <1578166061@qq.com>
2020-08-13 21:06:51 +0800

    通过让所有 schema 名字互不相同解决了多重解析 bug，有一个小问题：print sink 不生效。

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -23,43 +23,43 @@ import java.util.Properties;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        final String DDLCreateBase = "create table base ( \n"+
-                "id int primary key, \n"+
-                "location varchar(20) not null \n"+
-                ")WITH ( \n"+
-                "'connector' = 'kafka', \n"+
-                "'topic' = 'example', \n"+
-                "'properties.group.id' = 'testGroup', \n"+
-                "'scan.startup.mode' = 'latest-offset', \n"+
-                "'properties.bootstrap.servers' = 'localhost:9092', \n"+
-                "'format' = 'canal-json', \n"+
-                "'canal-json.ignore-parse-errors'='true'\n"+
+        final String DDLCreateBase = "create table base (\n" +
+                "\tbase_id int primary key,\n" +
+                "\tbase_location varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector' = 'kafka',\n" +
+                "\t'topic' = 'example',\n" +
+                "\t'properties.group.id' = 'testGroup',\n" +
+                "\t'scan.startup.mode' = 'latest-offset',\n" +
+                "\t'properties.bootstrap.servers' = 'localhost:9092',\n" +
+                "\t'format' = 'canal-json',\n" +
+                "\t'canal-json.ignore-parse-errors'='true'\n" +
                 ")";
-        final String DDLCreateStuff = "create table stuff(\n"+
-                "id int primary key,\n"+
-                "b_id int not null,\n"+
-                "name varchar(20) not null\n"+
-                ")WITH (\n"+
-                "'connector' = 'kafka',\n"+
-                "'topic' = 'example',\n"+
-                "'properties.group.id' = 'testGroup',\n"+
-                "'scan.startup.mode' = 'latest-offset',\n"+
-                "'properties.bootstrap.servers' = 'localhost:9092',\n"+
-                "'format' = 'canal-json', \n"+
-                "'canal-json.ignore-parse-errors'='true'\n"+
+        final String DDLCreateStuff = "create table stuff(\n" +
+                "\tstuff_id int primary key,\n" +
+                "\tstuff_base_id int,\n" +
+                "\tstuff_name varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector' = 'kafka',\n" +
+                "\t'topic' = 'example',\n" +
+                "\t'properties.group.id' = 'testGroup',\n" +
+                "\t'scan.startup.mode' = 'latest-offset',\n" +
+                "\t'properties.bootstrap.servers' = 'localhost:9092',\n" +
+                "\t'format' = 'canal-json',\n" +
+                "\t'canal-json.ignore-parse-errors'='true'\n" +
                 ")";
-        final String DDLCreateWideStuff = "create table wide_stuff(\n"+
-                "s_id int primary key,\n"+
-                "b_id int,\n"+
-                "b_location varchar(20),\n"+
-                "s_name varchar(20)\n"+
-                ") WITH (\n"+
-                "'connector'  = 'jdbc',\n"+
-                "'url'        = 'jdbc:mysql://127.0.0.1:4000/test',\n"+
-                "'table-name' = 'wide_stuff',\n"+
-                "'driver'     = 'com.mysql.cj.jdbc.Driver',\n"+
-                "'username'   = 'root',\n"+
-                "'password'   = ''\n"+
+        final String DDLCreateWideStuff = "create table wide_stuff(\n" +
+                "\tstuff_id int not null primary key,\n" +
+                "\tbase_id int,\n" +
+                "\tbase_location varchar(20),\n" +
+                "\tstuff_name varchar(20)\n" +
+                ") WITH (\n" +
+                "\t'connector'  = 'jdbc',\n" +
+                "\t'url'        = 'jdbc:mysql://127.0.0.1:4000/test',\n" +
+                "\t'table-name' = 'wide_stuff',\n" +
+                "\t'driver'     = 'com.mysql.cj.jdbc.Driver',\n" +
+                "\t'username'   = 'root',\n" +
+                "\t'password'   = ''\n" +
                 ")";
 
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(
@@ -71,29 +71,28 @@ public class Main {
         tEnv.executeSql(DDLCreateStuff);
         tEnv.executeSql(DDLCreateWideStuff);
 
-        printSource(tEnv, "base");
-        printSource(tEnv, "stuff");
+//        printSource(tEnv, "base");
+//        printSource(tEnv, "stuff");
 
         tEnv.executeSql("CREATE TABLE print_wide_stuff WITH ('connector' = 'print') LIKE wide_stuff (EXCLUDING ALL)");
-
         Table t = tEnv.sqlQuery(
-                "select distinct stuff.id s_id, base.id b_id, base.location, stuff.name\n" +
+                "select stuff.stuff_id, base.base_id, base.base_location, stuff.stuff_name\n" +
                 "from stuff inner join base\n" +
-                "on stuff.b_id = base.id\n" +
-                "where location is not null"
+                "on stuff.stuff_base_id = base.base_id"
         );
         t.executeInsert("wide_stuff");
         t.executeInsert("print_wide_stuff");
     }
-    /**
-     * 将源表的任何更改打印在屏幕上，原理是创建一个名为 print_${source}, sink = print 的表
-     * @param tEnv 表环境
-     * @param source 表名：必须已经被创建，必须是 source 表
-     */
-    static void printSource(StreamTableEnvironment tEnv, String source) {
-        tEnv.executeSql("CREATE TABLE print_" + source + " WITH ('connector' = 'print') LIKE " + source + " (EXCLUDING ALL)");
-        tEnv.from(source).executeInsert("print_" + source);
-    }
+
+//    /**
+//     * 将源表的任何更改打印在屏幕上，原理是创建一个名为 print_${source}, sink = print 的表
+//     * @param tEnv 表环境
+//     * @param source 表名：必须已经被创建，必须是 source 表
+//     */
+//    static void printSource(StreamTableEnvironment tEnv, String source) {
+//        tEnv.executeSql("CREATE TABLE print_" + source + " WITH ('connector' = 'print') LIKE " + source + " (EXCLUDING ALL)");
+//        tEnv.from(source).executeInsert("print_" + source);
+//    }
 }
 
 
@@ -108,6 +107,6 @@ insert into stuff values (1, 1, 'zz');
 insert into stuff values (2, 1, 't');
 insert into base values (2, 'shanghai');
 insert into stuff values (3, 2, 'qq');
-update stuff set name = 'qz' where id = 3;
-delete from stuff where name = 't';
+update stuff set stuff_name = 'qz' where stuff_id = 3;
+delete from stuff where stuff_name = 't';
 */
